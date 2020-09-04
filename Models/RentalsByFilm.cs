534e12ef33a0a638b4adf0f1d994966e60455dba
RTDistante <66966970+RTDistante@users.noreply.github.com>
2020-09-04 15:30:11 -0700

    Add files via upload

---
+++ Models/RentalsByFilm.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sakila.Models
+{
+    public partial class RentalsByFilm
+    {
+        public string FilmTitle { get; set; }
+        public int? TotalRentals { get; set; }
+    }
+}
---
+++ Models/TopSellingRRatedFilms.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sakila.Models
+{
+    public partial class TopSellingRRatedFilms
+    {
+        public string FilmTitle { get; set; }
+        public string Rating { get; set; }
+        public short? Length { get; set; }
+        public decimal? RentalSales { get; set; }
+    }
+}
